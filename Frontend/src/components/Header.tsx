2d0bd11ccb2d9049af45a94eb730cf7fed244363
Mohamed Jalloh <codingwithmbj@gmail.com>
2026-07-06 18:40:36 -0500

    installed recharts and update the case sensitivity for folder error

---
+++ Frontend/src/components/Header.tsx
@@ -0,0 +1,52 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useBreakpoint } from "../hooks/useBreakingPoint";
+import useTheme from "../hooks/useTheme";
+import Logo from "./Logo";
+import SearchForm from "./SearchForm";
+import UserBanner from "./UserBanner";
+
+import { faBars, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+
+interface HeaderProps {
+  openMenu: () => void;
+}
+
+const Header = ({ openMenu }: HeaderProps) => {
+  const { toggleTheme, darkTheme } = useTheme();
+  const { isDesktop } = useBreakpoint();
+
+  const theme = darkTheme ? faMoon : faSun;
+
+  return (
+    <header className="header shadow-(--bg-secondary)">
+      <div className="header-container p-3 flex justify-between items-center w-full">
+        <div className="header-left flex justify-between items-center gap-4 w-1/2">
+          {!isDesktop && (
+            <button className="menu-btn" onClick={openMenu}>
+              <FontAwesomeIcon
+                icon={faBars}
+                className="text-2xl cursor-pointer"
+              />
+            </button>
+          )}
+
+          {!isDesktop && <Logo />}
+          {isDesktop && <UserBanner />}
+        </div>
+
+        <div className="header-right flex items-center justify-end gap-4 lg:w-96">
+          {isDesktop && <SearchForm />}
+          <button className="theme-btn shrink-0">
+            <FontAwesomeIcon
+              icon={theme}
+              className="cursor-pointer"
+              onClick={toggleTheme}
+            />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
