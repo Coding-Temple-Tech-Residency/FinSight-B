d67efb6ac20e137cc0b2d85b7a2bdf3a6044c26f
Mohamed Jalloh <codingwithmbj@gmail.com>
2026-07-20 20:11:20 -0500

    updated portfolio to be able to edit description and verified the chat and fixed modal issues

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -4,9 +4,8 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import MobileDashboardNav from "../components/MobileDashboardNav";
 import SearchForm from "../components/SearchForm";
-
-import DashboardProvider from "../features/dashboard/providers/DashboardProvider";
 import { useBreakpoint } from "../hooks/useBreakingPoint";
+
 import "./DashboardLayout.css";
 
 interface DashboardLayoutProps {
@@ -15,7 +14,6 @@ interface DashboardLayoutProps {
 
 const DashboardLayout = ({ closeMenu }: DashboardLayoutProps) => {
   const { isDesktop } = useBreakpoint();
-
   const [showSearch, setShowSearch] = useState(false);
 
   const closeSearch = () => {
@@ -23,33 +21,31 @@ const DashboardLayout = ({ closeMenu }: DashboardLayoutProps) => {
   };
 
   return (
-    <DashboardProvider>
-      <div className="dashboard-layout">
-        <Header
-          closeMenu={closeMenu}
-          showSearch={showSearch}
-          setShowSearch={setShowSearch}
-        />
+    <div className="dashboard-layout">
+      <Header
+        closeMenu={closeMenu}
+        showSearch={showSearch}
+        setShowSearch={setShowSearch}
+      />
 
-        {showSearch && (
-          <div id="platform-search-row" className="search-row">
-            <SearchForm
-              closeSearch={closeSearch}
-              placeholder="Search the platform..."
-              autoFocus
-            />
-          </div>
-        )}
+      {showSearch && (
+        <div id="platform-search-row" className="search-row">
+          <SearchForm
+            closeSearch={closeSearch}
+            placeholder="Search the platform..."
+            autoFocus
+          />
+        </div>
+      )}
 
-        <main className="main">
-          <div className="main-container">
-            <Outlet />
-          </div>
-        </main>
+      <main className="main">
+        <div className="main-container">
+          <Outlet />
+        </div>
+      </main>
 
-        {!isDesktop && <MobileDashboardNav closeMenu={closeMenu} />}
-      </div>
-    </DashboardProvider>
+      {!isDesktop && <MobileDashboardNav closeMenu={closeMenu} />}
+    </div>
   );
 };
 
